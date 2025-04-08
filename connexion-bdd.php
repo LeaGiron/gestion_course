3f19c26561e9a731d3c43220ea4067160392d303
Léa <133007284+LeaGiron@users.noreply.github.com>
2025-04-08 21:04:54 +0200

    Update connexion-bdd.php

--- connexion-bdd.php
+++ connexion-bdd.php
@@ -1,11 +1,1 @@
-<?php
-$config = require 'config.php';
 
-try {
-    // Connexion à la base de données 
-    $pdo = new PDO("mysql:host=" . $config['host'] . ";dbname=" . $config['db'] . ";charset=utf8", $config['user'], $config['password']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-?>
